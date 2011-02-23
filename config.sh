effdc451f345ca3a374ba36424d588cd6dbbf052
laurelai <laurelai@debian.(none)>
2011-02-23 06:49:26 -0600

    created a proper build dir IAW debian build guide

--- config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-   lb config noauto \
-       --architecture i386 \
-       --linux-flavours 686 \
-       --packages-lists gnome \
-       --packages "iceweasel irssi nano vidalia perl gcc python ruby adduser aide apt-utils" \
-       "${@}"
